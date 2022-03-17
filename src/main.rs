fd1f0dc1ce24fbdb723c244f61484cc161597dc5
Scott Gifford <sggiffor@amazon.com>
2022-03-16 21:23:05 -0400

    Add simple command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,62 @@
-use std::io;
 use std::str::FromStr;
+use std::fmt;
 
 mod eq;
 mod expr;
 
 use crate::eq::Equation;
+use crate::expr::Expression;
 
-fn main() {
-    println!("Enter an Equation to parse");
+#[derive(Clone)]
+pub struct CommandLineError {
+    message: String,
+}
 
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+impl fmt::Display for CommandLineError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CommandLineError
+    : {}", self.message)
+    }
+}
 
-    println!("You inputed: {}", input);
+impl fmt::Debug for CommandLineError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: Line and file are this one, not caller?!
+        write!(f, "CommandLineError
+    : {} at {{ file: {}, line: {} }}", self.message, file!(), line!()) // programmer-facing output
+    }
+}
 
-    let eq = Equation::from_str(&input)
-        .expect("Failed to parse equation");
-    println!("Expression: {}", eq.to_string());
 
-    let res = eq.computes()
-        .expect("Failed to compute expression");
-    println!("Equation Computes: {}", res.to_string());
+fn main() -> Result<(), CommandLineError> {
+    let cmd = std::env::args().nth(1);
+    match cmd.as_deref() {
+        Some("expr") => {
+            let expr = std::env::args().nth(2)
+                .expect("no expr given");
+            let expr = Expression::from_str(&expr)
+                .expect("Failed to parse expression");
+            println!("Expression: {}", &expr);
+            let res = expr.calculate()
+                .expect("Failed to compute expression");
+            println!("Equation Computes: {}", res);
+            Ok(())
+        },
+
+        Some("eq") => {
+            let eq = std::env::args().nth(2)
+                .expect("no expr given");
+            let eq = Equation::from_str(&eq)
+                .expect("Failed to parse equation");
+            println!("Equation: {}", &eq);
+            let res = eq.computes()
+                .expect("Failed to compute expression");
+            println!("Equation Computes: {}", res);
+            Ok(())
+        },
+
+        Some(oops) => Err(CommandLineError { message: format!("Unrecognized command '{}'", oops) } ),
+
+        None => Err(CommandLineError { message: format!("Missing command line flag") } ),
+    }
 }
