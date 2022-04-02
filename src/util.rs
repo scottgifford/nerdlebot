33d121011897fbe59ba24f704d0ed1a1157e1572
Scott Gifford <sggiffor@amazon.com>
2022-04-02 17:45:33 -0400

    Make range_rand_or_only a generic function.

--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,24 @@
 use std::ops::RangeInclusive;
 use rand::Rng;
+use rand::distributions::uniform::SampleUniform;
 
 use crate::constraint::NoMatchFound;
 
-pub fn range_rand_or_only(range: &RangeInclusive<u32>) -> Result<u32, NoMatchFound> {
+pub fn range_rand_or_only<T>(range: RangeInclusive<T>) -> Result<T, NoMatchFound>
+where
+    T: PartialOrd,
+    T: std::fmt::Display,
+    T: SampleUniform,
+    T: Copy,
+{
     if range.is_empty() {
-        if (range.start() == range.end()) {
-            Ok(*range.start())
+        if range.start() == range.end() {
+            Ok(*(range.start()))
         } else {
             Err(NoMatchFound { message: format!("Could not choose item from range {}..={}", range.start(), range.end())})
         }
     } else {
         let mut rng = rand::thread_rng();
-        Ok(rng.gen_range(*range))
+        Ok(rng.gen_range(range))
     }
 }
