29e0edb6c7668afb29a44312b019da2e9755dab5
Scott Gifford <sggiffor@amazon.com>
2022-04-04 22:47:04 -0400

    Refactor eqgen operator selection into a single method to generate both operators, in preparation for making it smarter.

--- src/eqgen.rs
+++ src/eqgen.rs
@@ -28,27 +28,26 @@ pub fn eqgen_constrained(constraint: &EquationConstraint) -> Result<Equation, No
 
     for _try in 1..ATTEMPTS {
         let mut parts: Vec<ExpressionPart> = Vec::new();
-        let num_ops = range_rand_or_only(constraint.num_ops.clone())?;
 
-        let extra_ops = if num_ops > 1 {
-            1
-        } else {
-            0
+        let(op1, op2_opt) = gen_ops(constraint)?;
+        let op2_str = match &op2_opt {
+            Some(op) => op.to_string(),
+            None => "None".to_string()
         };
-        // println!("Trying with {} extra operators", extra_ops);
-        let operand_range = if extra_ops < 1 {
+
+        // println!("Trying with op1 {}, op2 {}", &op1, &op2_opt.map(|op| op.to_string()).unwrap_or("None"));
+        let operand_range = if op2_opt.is_none() {
             1..=NERDLE_A_MAX
         } else {
             1..=99
         };
 
-        let op1 = gen_operator_constrained(constraint);
         let a_base_range = match op1 {
-            ExpressionOperatorEnum::Divide if extra_ops < 1 => 100..=999,
+            ExpressionOperatorEnum::Divide if op2_opt.is_none() => 100..=999,
             _ => operand_range.clone()
         };
         let a_base_constraint = ExpressionNumberConstraint {
-            description: format!("{}..={} (from operator {}, extra_ops {})", a_base_range.start(), a_base_range.end(), op1, extra_ops),
+            description: format!("{}..={} (from op1 {}, op2 {})", a_base_range.start(), a_base_range.end(), &op1, &op2_str),
             range: a_base_range,
             ..Default::default()
         };
@@ -58,7 +57,7 @@ pub fn eqgen_constrained(constraint: &EquationConstraint) -> Result<Equation, No
 
         let mut b_base_constraint = ExpressionNumberConstraint {
             range: operand_range.clone(),
-            description: format!("{}..={} (from operator {}, extra_ops {})", operand_range.start(), operand_range.end(), op1, extra_ops),
+            description: format!("{}..={} (from op1 {}, op2 {})", operand_range.start(), operand_range.end(), &op1, &op2_str),
             ..Default::default()
         };
         match op1 {
@@ -84,17 +83,19 @@ pub fn eqgen_constrained(constraint: &EquationConstraint) -> Result<Equation, No
         parts.push(op2op(&op1));
         parts.push(ExpressionPart::Number(b));
 
-        for _i in 0..extra_ops {
-            let op2 = gen_operator_constrained(constraint);
-            parts.push(op2op(&op2));
+        match op2_opt {
+            Some(op2) => {
+                parts.push(op2op(&op2));
 
-            let b2_base_constraint = ExpressionNumberConstraint {
-                range: operand_range.clone(),
-                description: format!("{}..={}", operand_range.start(), operand_range.end()),
-                ..Default::default()
-            };
-            let b2 = find_num_with_constraint(&mut rng, &ExpressionNumberConstraint::intersect(&b2_base_constraint, &constraint.b2_constraint))?;
-            parts.push(ExpressionPart::Number(b2));
+                let b2_base_constraint = ExpressionNumberConstraint {
+                    range: operand_range.clone(),
+                    description: format!("{}..={}", operand_range.start(), operand_range.end()),
+                    ..Default::default()
+                };
+                let b2 = find_num_with_constraint(&mut rng, &ExpressionNumberConstraint::intersect(&b2_base_constraint, &constraint.b2_constraint))?;
+                parts.push(ExpressionPart::Number(b2));
+            },
+            None => { }
         }
 
         let expr = Expression { parts };
@@ -143,6 +144,57 @@ fn op2op (op: &ExpressionOperatorEnum) -> ExpressionPart {
 
 pub fn eqgen() -> Result<Equation, NoMatchFound> {
     eqgen_constrained(&EquationConstraint::default())
+}
+
+fn gen_ops(constraint: &EquationConstraint) -> Result<(ExpressionOperatorEnum, Option<ExpressionOperatorEnum>), NoMatchFound> {
+    // let must_have: Vec<u8> = constraint.operator.iter().filter(|(_op, info)| info.start() > &0).map(|(op, _info)| *op).collect();
+    // let may_have: Vec<u8> = constraint.operator.iter().filter(|(_op, info)| info.end() > &0).map(|(op, _info)| *op).collect();
+
+    // fn b2op(b: &u8) -> Result<ExpressionOperatorEnum, NoMatchFound> {
+    //     match ExpressionOperatorEnum::from_char_byte(&b) {
+    //         Ok(op) => Ok(op),
+    //         Err(err) => Err(NoMatchFound { message: format!("Invalid operator: {}", err)})
+    //     }
+    // }
+
+    let num_ops_range = &constraint.num_ops;
+    let num_ops = if num_ops_range.start() == num_ops_range.end() {
+        // We know how many there are
+        *(num_ops_range.start())
+    } else {
+        range_rand_or_only(constraint.num_ops.clone())?
+    };
+
+    return Ok((
+        gen_operator_constrained(&constraint),
+        if num_ops == 1 {
+            None
+        } else {
+            Some(gen_operator_constrained(&constraint))
+        }
+    ));
+    // let op1 = gen_operator_constrained(&constraint);
+    // let op
+    // if num_ops == 1 {
+    //     if must_have.len() > 0 {
+    //         // TODO: Randomly choose order
+    //         return Ok((b2op(&must_have[0])?, None));
+    //     } else {
+    //         return Ok((b2op(&may_have[0])?, None));
+    //     }
+    // } else {
+    //     // 2 operators
+    //     if must_have.len() == 2 {
+    //         // We can just choose the order
+    //         // TODO: Randomly choose order
+    //         return Ok((b2op(&must_have[0])?, Some(b2op(&must_have[1])?)));
+    //     } else if must_have.len() > 0 {
+    //         // TODO: Randomly choose order
+    //         return Ok((b2op(&must_have[0])?, Some(b2op(&may_have[0])?)));
+    //     } else {
+    //         return Ok((b2op(&may_have[0])?, Some(b2op(&may_have[1])?)));
+    //     }
+    // }
 }
 
 pub fn gen_operator_constrained(constraint: &EquationConstraint) -> ExpressionOperatorEnum {
