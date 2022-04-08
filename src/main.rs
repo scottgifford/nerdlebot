fb0e5797b85d3024cbce49901cb7daa33c9ed1f5
Scott Gifford <sggiffor@amazon.com>
2022-04-07 23:47:10 -0400

    Catch panics while solving, and add to stats instead of panicking overall.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fmt;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::time::{Instant};
+use std::panic;
 
 use colored::*;
 
@@ -225,7 +226,7 @@ fn main() -> Result<(), CommandLineError> {
 
         // TODO: Lots of duplicated code
         Some("solve_random") => {
-            let count = std::env::args().nth(2).map(|x| i32::from_str(&x).expect("Invalid number of turns")).unwrap_or(1);
+            let count = std::env::args().nth(2).map(|x| i32::from_str(&x).expect("Invalid number of games")).unwrap_or(1);
             let mut wins = 0;
             let mut losses = 0;
             let mut win_turn_hist = [0; nerdle::NERDLE_TURNS as usize];
@@ -234,40 +235,51 @@ fn main() -> Result<(), CommandLineError> {
                 println!("=== Playing game {} / {}", i, count);
                 let start_time = Instant::now();
 
-                let mut solver = NerdleSolver::new();
-                let answer = skip_fail!(eqgen(), "Failed to generate equation");
-                println!("Answer: {}", &answer);
+                let result = panic::catch_unwind(|| {
+                    let mut solver = NerdleSolver::new();
+                    let answer = eqgen().expect("Failed to generate equation");
+                    println!("Answer: {}", &answer);
 
-                let mut won = false;
-                for turn in 1..=nerdle::NERDLE_TURNS {
-                    let mut guess;
-                    let res;
+                    let mut turn: u32 = 0;
                     loop {
-                        guess = skip_fail!(solver.take_guess(), "No valid guess was generating, trying again");
-                        println!("Turn {}  Guess: {}", turn, guess);
-                        match solver.eq_matches(&guess) {
-                            Ok(()) => { },
-                            Err(why) => println!("Equation is impossible because {}", why)
+                        turn += 1;
+                        if turn > nerdle::NERDLE_TURNS {
+                            break GameResult::Loss();
                         }
-                        res = skip_fail!(nerdle::nerdle(&guess, &answer), "Nerdling failed, trying again");
-                        break;
-                    }
 
-                    println!("Turn {} Result: {}", turn, res);
-                    pretty_print_result(&guess.to_string(), &res);
-                    if res.won() {
-                        won = true;
+                        let mut guess;
+                        let res;
+                        loop {
+                            guess = skip_fail!(solver.take_guess(), "No valid guess was generating, trying again");
+                            println!("Turn {}  Guess: {}", turn, guess);
+                            match solver.eq_matches(&guess) {
+                                Ok(()) => { },
+                                Err(why) => println!("Equation is impossible because {}", why)
+                            }
+                            res = skip_fail!(nerdle::nerdle(&guess, &answer), "Nerdling failed, trying again");
+                            break;
+                        }
+
+                        println!("Turn {} Result: {}", turn, res);
+                        pretty_print_result(&guess.to_string(), &res);
+                        if res.won() {
+                            break GameResult::Win(turn);
+                        }
+                        solver.update(&guess, &res);
+                        solver.print_hint();
+                    }
+                });
+                match result {
+                    Ok(GameResult::Win(turn)) => {
                         println!("I won in {} turns!", turn);
                         wins += 1;
                         win_turn_hist[turn as usize - 1] += 1;
-                        break;
-                    }
-                    solver.update(&guess, &res);
-                    solver.print_hint();
-                }
-                if !won {
-                    losses += 1;
-                    println!("I lost");
+                    },
+                    Ok(GameResult::Loss()) => {
+                        losses += 1;
+                        println!("I lost");
+                    },
+                    Err(err) => println!("Game failed: {:?}", err)
                 }
                 println!("Game {} completed in {:?}", i, start_time.elapsed());
             }
@@ -285,62 +297,68 @@ fn main() -> Result<(), CommandLineError> {
 
         // TODO: Lots of duplicated code
         Some("solve") => {
-            let answer = std::env::args().nth(2)
-                .expect("no expr given");
-            let answer = Equation::from_str(&answer)
-                .expect("Failed to parse equation");
+            let result = panic::catch_unwind(|| {
+                let answer = std::env::args().nth(2)
+                    .expect("no expr given");
+                let answer = Equation::from_str(&answer)
+                    .expect("Failed to parse equation");
 
-            if answer.len() != NERDLE_CHARACTERS as usize {
-                return Err(CommandLineError { message: format!("Equation '{}' is wrong length ({} chars != {})", answer, answer.len(), NERDLE_CHARACTERS) } );
-            }
-            if !answer.computes().unwrap_or(false) {
-                return Err(CommandLineError { message: format!("Equation unexpectedly did not compute: {}", answer) } );
-            }
+                if answer.len() != NERDLE_CHARACTERS as usize {
+                    return Err(CommandLineError { message: format!("Equation '{}' is wrong length ({} chars != {})", answer, answer.len(), NERDLE_CHARACTERS) } );
+                }
+                if !answer.computes().unwrap_or(false) {
+                    return Err(CommandLineError { message: format!("Equation unexpectedly did not compute: {}", answer) } );
+                }
         
-            let mut solver = NerdleSolver::new();
-            println!("Answer: {}", &answer);
+                let mut solver = NerdleSolver::new();
+                println!("Answer: {}", &answer);
 
-            let mut won = false;
-            for turn in 1..=nerdle::NERDLE_TURNS {
-                let mut guess;
-                let res;
-                loop {
-                    let constraint = solver.constraint();
-                    match constraint.accept(&answer) {
-                        Err(err) =>  return Err(CommandLineError { message: format!("Solver constraint {} rejects answer: {}", constraint, err) } ),
-                        Ok(()) => { }
-                    }
-                    guess = match std::env::args().nth(2 + turn as usize) {
-                        Some(guess) => match Equation::from_str(&guess) {
-                            Ok(guess) => guess,
-                            Err(err) => return Err(CommandLineError { message: format!("Invalid guess equation in command-line arg {} '{}': {}", 2+turn, guess, err) } )
+                let mut won = false;
+                for turn in 1..=nerdle::NERDLE_TURNS {
+                    let mut guess;
+                    let res;
+                    loop {
+                        let constraint = solver.constraint();
+                        match constraint.accept(&answer) {
+                            Err(err) =>  return Err(CommandLineError { message: format!("Solver constraint {} rejects answer: {}", constraint, err) } ),
+                            Ok(()) => { }
                         }
-                        None => skip_fail!(solver.take_guess(), "No valid guess was generating, trying again")
-                    };
-                    println!("Turn {}  Guess: {}", turn, guess);
-                    match solver.eq_matches(&guess) {
-                        Ok(()) => { },
-                        Err(why) => println!("Equation is impossible because {}", why)
+                        guess = match std::env::args().nth(2 + turn as usize) {
+                            Some(guess) => match Equation::from_str(&guess) {
+                                Ok(guess) => guess,
+                                Err(err) => return Err(CommandLineError { message: format!("Invalid guess equation in command-line arg {} '{}': {}", 2+turn, guess, err) } )
+                            }
+                            None => skip_fail!(solver.take_guess(), "No valid guess was generating, trying again")
+                        };
+                        println!("Turn {}  Guess: {}", turn, guess);
+                        match solver.eq_matches(&guess) {
+                            Ok(()) => { },
+                            Err(why) => println!("Equation is impossible because {}", why)
+                        }
+                        res = skip_fail!(nerdle::nerdle(&guess, &answer), "Nerdling failed, trying again");
+                        break;
                     }
-                    res = skip_fail!(nerdle::nerdle(&guess, &answer), "Nerdling failed, trying again");
-                    break;
-                }
 
-                println!("Turn {} Result: {}", turn, res);
-                pretty_print_result(&guess.to_string(), &res);
-                if res.won() {
-                    won = true;
-                    println!("I won in {} turns!", turn);
-                    break;
+                    println!("Turn {} Result: {}", turn, res);
+                    pretty_print_result(&guess.to_string(), &res);
+                    if res.won() {
+                        won = true;
+                        println!("I won in {} turns!", turn);
+                        break;
+                    }
+                    solver.update(&guess, &res);
+                    solver.print_hint();
                 }
-                solver.update(&guess, &res);
-                solver.print_hint();
+                if !won {
+                    println!("I lost");
+                }
+                Ok(())
+            });
+            match result {
+                Ok(Ok(())) => Ok(()),
+                Ok(Err(err)) => Err(CommandLineError { message: format!("Failed: {:?}", err) }),
+                Err(err) => Err(CommandLineError { message: format!("Panicked: {:?}", err) })
             }
-            if !won {
-                println!("I lost");
-            }
-
-            Ok(())
         },
 
         // TODO: Lots of duplicated code
@@ -488,4 +506,9 @@ fn main() -> Result<(), CommandLineError> {
 
         None => Err(CommandLineError { message: format!("Missing command line flag") } ),
     }
+}
+
+enum GameResult {
+    Win(u32),
+    Loss(),
 }
